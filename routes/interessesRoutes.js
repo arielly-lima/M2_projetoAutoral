3eb18c2fff303d2fd0f024f08dc28c4202edd186
Arielly <maria.lima@sou.inteli.edu.br>
2025-06-08 11:09:05 -0300

    feat: tentando adicionar view interesses

--- routes/interessesRoutes.js
+++ routes/interessesRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const interesseController = require('../controllers/interessesController');
 
+//Criar interesse
+router.post('/novointeresse', (req, res) => {
+  //view
+   res.render('pages/interesses', { erro: null });
+});
+
 // Criar interesse
-router.post('/novointeresses', interesseController.criarInteresse);
+router.post('/novointeresse', interesseController.criarInteresse);
 
 // Listar interesses
 router.get('/interesses', interesseController.listarInteresses);
